9d8ae0ce6c314d1568d61a968ffbbdcfa6c1eb85
Sergey Kastryulin <snk4tr@gmail.com>
2019-02-10 19:36:05 +0300

    docs(decs): add a bit more documentation

--- decs/testing.py
+++ decs/testing.py
@@ -7,15 +7,29 @@ def repeat(times):
 
     Warnings:
         Can be applied only for function with no return value.
+        Otherwise the return value will be lost.
+
+    Examples:
+        This decorator primary purpose is to repeat execution of some test function multiple times:
+
+        import random
+
+        @repeat(10)
+        def test_int_is_taken():
+            examples = list(range(10)
+            assert type(random.choice(examples)) == int, 'Error!'
+
+        # No error, test is repeated 10 times.
 
     Args:
         times(int): required number of times for the function to be repeated.
 
     Returns:
-        (None)
+        None
     """
 
     def repeat_helper(func):
+
         @functools.wraps(func)
         def call_helper(*args):
             for i in range(times):
--- decs/verification.py
+++ decs/verification.py
@@ -8,10 +8,34 @@ def accepts(*types):
     Idea is taken from: https://www.python.org/dev/peps/pep-0318/
 
     Args:
-        *types(list/tuple): types of funtion' parameters.
+        *types(list/tuple): types of parameters of the function.
 
     Returns:
-        (function): initial funtion.
+        (function): initial function.
+
+    Examples:
+        Functions types of one or many args or kwargs can be verified:
+
+        @accepts(str)
+        def my_print(string):
+            print(string)
+            return string
+
+        # if type(string) == str passes else raises AssertionError.
+
+
+        Class methods types of one or many args or kwargs can be verified:
+
+        @accepts('self', str)
+        def my_print_method(self, string):
+            print(string)
+            return string
+
+        # if type(string) == str passes else raises AssertionError.
+
+    Raises:
+        AssertionError: if one or more passed types are not equal to the *args, **kwargs types
+            (element-wise comparision).
     """
 
     def check_accepts(func):
@@ -50,6 +74,30 @@ def returns(rtype):
 
     Returns:
         (function): initial funtion.
+
+    Examples:
+        Return value of the function can be verified the following way:
+
+        @returns(int)
+        def my_function():
+            return 42
+
+        # No error
+
+        @returns(int)
+        def my_function():
+            return '42'
+
+        # AssertionError
+
+        @returns(tuple)
+        def my_function():
+            return 42, 24
+
+        # No error
+
+    Raises:
+        AssertionError: if type of return value is not equal to the type passed to the decorator.
     """
 
     def wraps(func):
